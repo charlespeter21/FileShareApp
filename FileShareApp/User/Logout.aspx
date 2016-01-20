ae18050730b01397c38a10883f7469848353671e
charlespeter21 <charles.peter21@gmail.com>
2016-01-20 23:50:51 +0400

    Adding Logout.aspx

---
+++ FileShareApp/User/Logout.aspx
@@ -0,0 +1,7 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/User/UserHeader.Master" AutoEventWireup="true" CodeBehind="Logout.aspx.cs" Inherits="FileShareApp.User.WebForm1" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="ContentPlaceHolder2" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content3" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+</asp:Content>
---
+++ FileShareApp/User/Logout.aspx.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Web.Services;
+
+namespace FileShareApp.User
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            Session.Abandon();
+            Session.Clear();
+            Session.RemoveAll();
+            System.Web.Security.FormsAuthentication.SignOut();
+            //Response.Redirect("frmLogin.aspx", false);
+            Response.Redirect("../Index.aspx",false);
+        }
+        [WebMethod]
+        public static int LogoutCheck()
+        {
+            if (HttpContext.Current.Session["user"] == null)
+            {
+                return 0;
+            }
+            return 1;
+        }
+    }
+}
